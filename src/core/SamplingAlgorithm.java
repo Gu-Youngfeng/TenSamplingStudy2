15d3b7afccb0691bf341d25f9c8497f25ec16f19
yongfeng <yongfeng_gu@163.com>
2018-11-30 16:37:20 +0800

    add comments on SamplingAlgorithm.java

--- src/core/SamplingAlgorithm.java
+++ src/core/SamplingAlgorithm.java
@@ -8,10 +8,23 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+/***
+ * <p><b>SamplingAlgorithm</b> 类是一个抽象类，所有的 core.algorithm 内的算法都继承自该类。</p>
+ * @editor yongfeng
+ * @see#getSamples
+ * @see#getDirectives
+ * @see#getNumberOfMacrosEnabled
+ */
 public abstract class SamplingAlgorithm {
 
 	protected List<String> directives;
 	
+	/**
+	 * <p>利用采样算法获取采样的配置组合，该方法为抽样方法需要被子类实现。</p>
+	 * @param file C 文件
+	 * @return 满足条件的配置组合的集合
+	 * @throws Exception
+	 */
 	public abstract List<List<String>> getSamples(File file) throws Exception;
 	
 	public boolean isValidJavaIdentifier(String s) {
@@ -39,6 +52,11 @@ public abstract class SamplingAlgorithm {
 	}
 	
 	// It sets the number of configurations..
+	/**
+	 * <p>获取 C 文件中配置项的个数</p>
+	 * @param file C 文件路径
+	 * 
+	 */
 	public List<String> getDirectives(File file) throws Exception{
 		List<String> directives = new ArrayList<>();
 		
@@ -94,6 +112,11 @@ public abstract class SamplingAlgorithm {
 		return directives;
 	}
 	
+	/**
+	 * <p>判断一个配置组合中有多少个配置项是启用的</p>
+	 * @param configuration 配置组合
+	 * @return 启用的配置项的个数
+	 */
 	public int getNumberOfMacrosEnabled(List<String> configuration){
 		int count = 0;
 		for (String macro : configuration){
